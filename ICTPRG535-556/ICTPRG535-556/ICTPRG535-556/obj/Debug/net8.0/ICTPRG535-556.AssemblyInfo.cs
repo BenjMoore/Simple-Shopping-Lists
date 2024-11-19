402b22943ce6c2ad27c7068409953235541936e0
Benjamin Moore <moorebenjamin30@gmail.com>
2024-11-19 21:36:37 +1000

    push

--- ICTPRG535-556/ICTPRG535-556/ICTPRG535-556/obj/Debug/net8.0/ICTPRG535-556.AssemblyInfo.cs
+++ ICTPRG535-556/ICTPRG535-556/ICTPRG535-556/obj/Debug/net8.0/ICTPRG535-556.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICTPRG535-556")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e65ce1fcc1a6f175894d08a55802bb8f59fc90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e77fcf9dbf322d11246d1681bb79483cbb3400")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICTPRG535-556")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICTPRG535-556")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
